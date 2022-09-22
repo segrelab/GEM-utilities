77d2f18b69e9025e5159267bdebf36e84d42af38
Helen Scott <helengracescott@gmail.com>
2022-09-22 16:48:14 -0400

    Skeleton test

---
+++ test/test_names.py
@@ -0,0 +1,21 @@
+import unittest
+import tempfile
+import filecmp
+
+import cobra
+
+from gem_utils import names
+
+
+class TestNames(unittest.TestCase):
+    def test_find_names_w_compartment_suffix(self):
+        """Test find_names_w_compartment_suffix function."""
+        # Read in the E coli core model
+        model = cobra.io.read_sbml_model('test_files/e_coli_core_missing_formulas.xml')
+
+        # Find metabolites with a compartment suffix
+        names_w_compartment_suffix = names.find_names_w_compartment_suffix(model)
+
+        # Assert that the correct metabolites are found
+        self.assertEqual(len(names_w_compartment_suffix), 2)
+        self.assertIn('ac_e', names_w_compartment_suffix)
