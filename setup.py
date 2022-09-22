6b5722d44ea659501525993f759969acec4e5903
Helen G Scott <helengracescott@gmail.com>
2022-09-22 17:20:51 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='GEM-utils',
+    version='0.1dev',
+    packages=['gem_utils',],
+    license='MIT License',
+    long_description=open('README.md').read(),
+)
