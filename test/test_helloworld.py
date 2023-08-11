939eb1b4782bc962d3e6b46a166cb659d4773580
hui-shi <huishi@bu.edu>
2023-08-11 10:48:24 -0400

    changed hello world

--- test/test_helloworld.py
+++ test/test_helloworld.py
@@ -3,6 +3,6 @@ from gem_utilities import helloworld
 class MyTestCase(unittest.TestCase):
     def test_default_greeting_set(self):
         greeter = helloworld.Greeter()
-        self.assertEqual(greeter.message, 'Hello world!')
+        self.assertEqual(greeter.message, 'Hello World')
 if __name__ == '__main__':
     unittest.main()
